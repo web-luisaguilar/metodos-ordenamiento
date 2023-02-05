0cafc8d3db739ce9abdb02550edfb2490cb6a3d5
web-luisaguilar <web.luisaguilar@gmail.com>
2023-02-04 22:52:17 -0600

    log prueba modules

---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict'
+import bubbleSort from './metodos/bubble-sort.js'
+
+bubbleSort()
---
+++ metodos/bubble-sort.js
@@ -0,0 +1,6 @@
+const bubbleSort = () => {
+  console.clear()
+  console.log('Ordenamiento por Metodo Bubble Sort')
+}
+
+export default bubbleSort
